98944ce822be490ac4f2236b2f5150563174bc4d
Iryna Hanuliak <hanuliak.iryna@gmail.com>
2021-12-07 23:37:51 +0100

    add exceptions

---
+++ 2021-12-07/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+
+    public static void main(String[] args) {
+        CustomArrayList myList = new CustomArrayList();
+        myList.add(2);
+        myList.add(4);
+        myList.add(6);
+        myList.println();
+        System.out.println(myList.size());
+        myList.set(1, 99);
+        myList.println();
+        System.out.println(myList.get(2));
+        System.out.println(myList.contains(2));
+        System.out.println(myList.contains(0));
+
+        myList.insert(2,44);
+        myList.println();
+        System.out.println(myList.size());
+
+        myList.add(2);
+        myList.add(4);
+        myList.add(6);
+        myList.add(7);
+        myList.add(7);
+        myList.add(9);
+        myList.println();
+        System.out.println(myList.size());
+        myList.removeById(0);
+        myList.println();
+        System.out.println(myList.size());
+        AdvancedArrayList myList2 = new AdvancedArrayList();
+        myList2.add(2);
+        myList2.add(4);
+        myList2.add(6);
+        System.out.println(myList2.size());
+        myList2.removeById(1);
+        myList2.println();
+    }
+}
